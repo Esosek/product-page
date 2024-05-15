f75e0c2dc4d88b835691e7614af10075b5b92a7c
Ales Zima <ales@cpex.cz>
2024-05-15 16:06:41 +0200

    Renamed props interface NavLink

--- src/components/header/NavLink.tsx
+++ src/components/header/NavLink.tsx
@@ -1,9 +1,9 @@
-interface NavLinkProps {
+interface INavLinkProps {
   label: string;
   target?: string;
 }
 
-export default function NavLink({ label, target = '#' }: NavLinkProps) {
+export default function NavLink({ label, target = '#' }: INavLinkProps) {
   return (
     <li className="text-neutral-700 border-b-4 pb-8 border-transparent has-[:hover]:text-neutral-900 has-[:hover]:border-primary-400 transition-colors">
       <a href={target}>{label}</a>
