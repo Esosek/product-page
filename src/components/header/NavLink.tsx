9c2c5c985c6272d9c4bc3eaf67dbdba055553fd7
Ales Zima <ales@cpex.cz>
2024-05-17 15:16:07 +0200

    Added mobile Navbar

--- src/components/header/NavLink.tsx
+++ src/components/header/NavLink.tsx
@@ -5,7 +5,7 @@ interface INavLinkProps {
 
 export default function NavLink({ label, target = '#' }: INavLinkProps) {
   return (
-    <li className="text-neutral-700 border-b-4 pt-10 pb-8 border-transparent has-[:hover]:text-neutral-900 has-[:hover]:border-primary-400 transition-colors">
+    <li className=" border-b-4 border-transparent font-bold md:font-normal md:text-neutral-700 md:pt-10 md:pb-8 has-[:hover]:text-neutral-900 has-[:hover]:border-primary-400 transition-colors">
       <a href={target}>{label}</a>
     </li>
   );
--- src/components/header/Navbar.tsx
+++ src/components/header/Navbar.tsx
@@ -1,17 +1,56 @@
+import { useEffect, useRef, useState } from 'react';
+
 import NavLink from './NavLink';
+
+import closeIcon from '../../assets/images/icon-close.svg';
+import menuIcon from '../../assets/images/icon-menu.svg';
 
 interface INavbarProps {}
 
 export default function Navbar(props: INavbarProps) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const backdropRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (isOpen) {
+      backdropRef.current!.style.display = '';
+      backdropRef.current!.style.opacity = '1';
+    } else {
+      backdropRef.current!.style.opacity = '0';
+      setTimeout(() => {
+        backdropRef.current!.style.display = 'none';
+      }, 500);
+    }
+  }, [isOpen]);
+
   return (
-    <nav>
-      <ul className="flex gap-8">
-        <NavLink label="Collections" />
-        <NavLink label="Men" />
-        <NavLink label="Women" />
-        <NavLink label="About" />
-        <NavLink label="Contact" />
-      </ul>
-    </nav>
+    <>
+      <div
+        ref={backdropRef}
+        className={
+          'fixed top-0 right-0 bottom-0 left-0 bg-black/75 md:hidden transition-opacity duration-500'
+        }
+      ></div>
+      <button onClick={() => setIsOpen(true)} className="-order-1 md:hidden">
+        <img src={menuIcon.src} alt="Menu icon" />
+      </button>
+      <nav
+        className={`${
+          !isOpen && '-translate-x-full'
+        } fixed top-0 bottom-0 left-0 w-2/3 bg-white z-30 p-6 transition-transform duration-500 md:p-0 md:static md:translate-x-0 md:transition-none`}
+      >
+        <button onClick={() => setIsOpen(false)} className="md:hidden pb-14">
+          <img src={closeIcon.src} alt="Close icon" />
+        </button>
+        <ul className="flex flex-col gap-4 md:gap-8 md:flex-row">
+          <NavLink label="Collections" />
+          <NavLink label="Men" />
+          <NavLink label="Women" />
+          <NavLink label="About" />
+          <NavLink label="Contact" />
+        </ul>
+      </nav>
+    </>
   );
 }
