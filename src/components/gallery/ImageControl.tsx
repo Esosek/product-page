60743c05675b2cf47e784734112ccdfca4792366
Ales Zima <ales@cpex.cz>
2024-05-28 13:25:23 +0200

    Fixed strokeWidth property

--- src/components/gallery/ImageControl.tsx
+++ src/components/gallery/ImageControl.tsx
@@ -41,7 +41,7 @@ export default function ImageControl(props: ImageControlProps) {
           <path
             d={props.isRight ? 'm2 1 8 8-8 8' : 'M11 1 3 9l8 8'}
             stroke={'#1D2026'}
-            stroke-width="3"
+            strokeWidth="3"
             fill="none"
             fillRule="evenodd"
             className="group-hover:stroke-primary-400 transition-colors"
