8185b6fc03939c4ee238d6297fddc93f87992240
Ales Zima <ales@cpex.cz>
2024-05-29 10:14:26 +0200

    Completed Cart UI

--- src/components/cart/Cart.tsx
+++ src/components/cart/Cart.tsx
@@ -1,9 +1,11 @@
 import { useContext, useState } from 'react';
 import { CartContext } from '../../context/CartContext';
+import PrimaryButton from '../PrimaryButton';
 
 export default function Cart() {
   const [isShown, setIsShown] = useState(false);
   const cartContext = useContext(CartContext);
+
   return (
     <>
       <button onClick={() => setIsShown(!isShown)} className="block ml-auto">
@@ -26,7 +28,50 @@ export default function Cart() {
           <h2 className="font-bold border-neutral-400 border-b-[1px] p-4">
             Cart
           </h2>
-          {cartContext.products.length === 0 && (
+
+          {cartContext.products.length !== 0 ? (
+            <div className="flex flex-col justify-between p-4 flex-grow">
+              <ul>
+                {cartContext.products.map((product) => {
+                  return (
+                    <li
+                      key={product.id}
+                      className="grid grid-cols-[2fr_auto_1fr] justify-items-center items-center gap-4"
+                    >
+                      <img
+                        src={product.images[0].thumbnail}
+                        alt="Product thumbnail"
+                        className="rounded-md"
+                      />
+                      <div className="text-neutral-700 text-sm">
+                        <p>{product.name}</p>
+                        <p>
+                          {`$${product.price.toFixed(2)} x ${product.quantity}`}{' '}
+                          <span className="font-bold text-neutral-900">
+                            ${(product.price * product.quantity).toFixed(2)}
+                          </span>
+                        </p>
+                      </div>
+                      <button
+                        onClick={() => cartContext.removeProduct(product)}
+                      >
+                        <svg
+                          width="14"
+                          height="16"
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="fill-neutral-400 hover:fill-primary-400 transition-colors"
+                        >
+                          <path d="M0 2.625V1.75C0 1.334.334 1 .75 1h3.5l.294-.584A.741.741 0 0 1 5.213 0h3.571a.75.75 0 0 1 .672.416L9.75 1h3.5c.416 0 .75.334.75.75v.875a.376.376 0 0 1-.375.375H.375A.376.376 0 0 1 0 2.625Zm13 1.75V14.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 1 14.5V4.375C1 4.169 1.169 4 1.375 4h11.25c.206 0 .375.169.375.375ZM4.5 6.5c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Z" />
+                        </svg>
+                      </button>
+                    </li>
+                  );
+                })}
+              </ul>
+              <PrimaryButton>Checkout</PrimaryButton>
+            </div>
+          ) : (
+            // Cart is empty
             <div className="flex-grow content-center">
               <p className="flex-grow text-center text-neutral-700">
                 Your cart is empty.
--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -3,14 +3,14 @@ import type { ProductType } from '../data/products';
 
 type CartContextType = {
   products: ProductType[];
-  addProduct: (product: ProductType) => void;
-  clear: () => void;
+  addProduct: (product: ProductType, quantity: number) => void;
+  removeProduct: (product: ProductType) => void;
 };
 
 const defaultValue: CartContextType = {
   products: [],
   addProduct: () => {},
-  clear: () => {},
+  removeProduct: () => {},
 };
 
 export const CartContext = createContext(defaultValue);
@@ -18,7 +18,7 @@ export const CartContext = createContext(defaultValue);
 export default function CartContextProvider({ children }: PropsWithChildren) {
   const [products, setProducts] = useState<ProductType[]>([]);
 
-  function addProduct(product: ProductType) {
+  function addProduct(product: ProductType, quantity: number = 1) {
     setProducts((prevValue) => {
       const existingProductIndex = prevValue.findIndex(
         (p) => p.id === product.id
@@ -27,19 +27,23 @@ export default function CartContextProvider({ children }: PropsWithChildren) {
         const updatedProducts = [...products];
         updatedProducts[existingProductIndex] = {
           ...updatedProducts[existingProductIndex],
-          quantity: updatedProducts[existingProductIndex].quantity + 1,
+          quantity: updatedProducts[existingProductIndex].quantity + quantity,
         };
         return updatedProducts;
       } else {
-        return [...prevValue, { ...product, quantity: 1 }];
+        return [...prevValue, { ...product, quantity: quantity }];
       }
     });
   }
 
+  function removeProduct(product: ProductType) {
+    setProducts((prevValue) => {
+      return [...prevValue.filter((p) => p.id !== product.id)];
+    });
+  }
+
   return (
-    <CartContext.Provider
-      value={{ products, addProduct, clear: () => setProducts([]) }}
-    >
+    <CartContext.Provider value={{ products, addProduct, removeProduct }}>
       {children}
     </CartContext.Provider>
   );
